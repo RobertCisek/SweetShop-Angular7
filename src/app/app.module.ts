6f4623b5af2b49c9f1db5523abc40d0496b98652
torqqq <r.cisek@o2.pl>
2018-11-10 22:10:32 +0100

    Added firebase, bootstrap and bootstrap navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import { environment } from 'src/environments/environment';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAKCS0gPZ0jhpY-hamVurpZ5B_gXIgzQw4",
+    authDomain: "sweetpage-01.firebaseapp.com",
+    databaseURL: "https://sweetpage-01.firebaseio.com",
+    projectId: "sweetpage-01",
+    storageBucket: "sweetpage-01.appspot.com",
+    messagingSenderId: "1074258719073"
+  }
 };
 
 /*
