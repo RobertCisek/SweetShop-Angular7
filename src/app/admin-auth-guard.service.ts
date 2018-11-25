5d0e284dfa07fd8965e9c4345f3096038b146df4
torqqq <r.cisek@o2.pl>
2018-11-25 20:35:54 +0100

    Hiding the Admin Links step 1

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router/src/utils/preactivation';
 import { AuthService } from './auth.service';
 import { UserService } from './user.service';
 import { switchMap, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { CanActivate } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminAuthGuardService implements CanActivate{
+  path: import("c:/Users/user/Desktop/ROBERT/SweetPage-Angular7/node_modules/@angular/router/src/router_state").ActivatedRouteSnapshot[];
+  route: import("c:/Users/user/Desktop/ROBERT/SweetPage-Angular7/node_modules/@angular/router/src/router_state").ActivatedRouteSnapshot;
 
   constructor(private auth: AuthService, private userService: UserService) { }
 
   canActivate(): Observable<boolean> { 
-    return this.auth.user$.pipe(    
-          switchMap(user => this.userService.get(user.uid).valueChanges()),    
+    return this.auth.appUser$.pipe(
           map(appUser => appUser.isAdmin)   
         )
-    
     }
 
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,15 +1,20 @@
+import { UserService } from './user.service';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import { timingSafeEqual } from 'crypto';
+import { AppUser } from './models/app-user';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
 user$: Observable<firebase.User>
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth, 
+    private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
    }
    login () {
@@ -22,4 +27,9 @@ user$: Observable<firebase.User>
   logout () {
     this.afAuth.auth.signOut();
   };
+
+  get appUser$() : Observable<AppUser> {
+    return this.user$.pipe(    
+      switchMap(user => this.userService.get(user.uid).valueChanges()),  
+    )}
 }
