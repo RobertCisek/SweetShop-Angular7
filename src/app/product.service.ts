e066f8447ba1f133b6996059170d1351736f3e80
torqqq <r.cisek@o2.pl>
2018-12-01 23:03:20 +0100

    Updating a Product

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -20,4 +20,7 @@ export class ProductService {
   get(productId){
     return this.db.object('/products/' + productId);
   }
+  update (productId, product) {
+    return this.db.object('/products/' + productId).update(product);
+  }
 }
